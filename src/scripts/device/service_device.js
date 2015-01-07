6f36d7fc249ebfcd6dab570b870fea774fe1bec7
Ian Duffy <ian@ianduffy.ie>
2015-01-07 02:23:14 +0000

    initial seperation of the carcloud ui elements

---
+++ src/scripts/device/service_device.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// SPAM 0.0.1 - Example of facade on $resource
+
+carcloudApp.factory('Device', function ($resource) {
+    return $resource('app/rest/devices/:id', {}, {
+        'query': {method: 'GET', isArray: true},
+        'get': {method: 'GET'},
+        'update': {method: 'PUT'}
+    });
+});
